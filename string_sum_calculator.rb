b47de3f7effcd62a156d8fa3916d4650ffb5abb3
mahadev <mahadev.patel1227@gmail.com>
2024-03-08 12:10:01 +0530

    Add tests for new lines and custom delimiters

--- string_sum_calculator.rb
+++ string_sum_calculator.rb
@@ -17,6 +17,14 @@ class StringCalculatorTest < Minitest::Test
   def test_add_multiple_numbers
     assert_equal 6, StringCalculator.add("1,2,3")
   end
+
+  def test_add_with_new_lines
+    assert_equal 6, StringCalculator.add("1\n2,3")
+  end
+
+  def test_add_with_different_delimiters
+    assert_equal 3, StringCalculator.add("//;\n1;2")
+  end
 end
 
 class StringCalculator
